9a514178aa8f94f16ea2ed8bd72fee9a6a7e61d6
SharkovKirill <55428474+SharkovKirill@users.noreply.github.com>
2021-09-29 18:17:20 +0700

    All is done

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import com.company.PhoneBook;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    PhoneBook pb = new PhoneBook();
+        pb.createRecord(new Record(1, "8", "Kirill"));
+        pb.createRecord(new Record(2, "8", "Kirill"));
+        pb.createRecord(new Record(2, "89", "Kirill"));
+        List<Record> list = pb.getAllRecords();
+        System.out.println(list);
+        pb.updateRecord(new Record(111, "8", "Kirill"));
+        pb.updateRecord(new Record(1, "98898", "Kir"));
+        pb.deleteRecord(77);
+    }
+}
